912c31cbc0e12bf7f0d32143a2565b24d170660d
Ash Ryan <ash@ashryanbeats.com>
2015-03-28 00:16:10 -0400

    Turned off hello-world demo and added notes on triggering synth from console.

--- public/js/main.js
+++ public/js/main.js
@@ -4,11 +4,24 @@ var synth = new Tone.MonoSynth();
 //connect the synth to the master output channel
 synth.toMaster();
 
-//create a callback which is invoked every quarter note
-Tone.Transport.setInterval(function(time){
-    //trigger middle C for the duration of an 8th note
-    synth.triggerAttackRelease("C4", "8n", time);
-}, "4n");
+// // 1. AUTO PLAY A NOTE EVERY QUARTER NOTE
+// //create a callback which is invoked every quarter note
+// Tone.Transport.setInterval(function(time){
+//     //trigger middle C for the duration of an 8th note
+//     synth.triggerAttackRelease("C4", "8n", time);
+// }, "4n");
 
-//start the transport
-Tone.Transport.start();
+// //start the transport
+// Tone.Transport.start();
+
+//trigger the start of a note.
+// synth.triggerAttack("C4");
+
+
+
+// // 2. CONSOLE FUN: use the commands below in your browser console to trigger and release notes
+// synth.triggerAttack("C4");
+// synth.triggerRelease();
+
+// // OR
+// synth.triggerAttackRelease("C4", 0.25);
